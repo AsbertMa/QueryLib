8649d87f1678a173f7dbd303f9d2f88291f79a8e
AsbertMa <stayinalive@msn.cn>
2023-06-09 11:46:57 +0800

    add block list

--- src/graphql/query/block.ts
+++ src/graphql/query/block.ts
@@ -1,4 +1,5 @@
-import { extendType, nonNull, stringArg } from "nexus"
+import { arg, extendType, intArg, nonNull, stringArg } from "nexus"
+import { Order } from "../type"
 export const blockById = extendType({
   type: 'Query',
   definition(t) {
@@ -14,10 +15,54 @@ export const blockById = extendType({
             id
           },
           include: {
-            txs: []
+            txs: true
           }
         })
       }
     })
   }
+})
+
+export const blocks = extendType({
+  type: 'Query',
+  definition(t) {
+    t.nullable.field('blocks', {
+      type: 'Blocks',
+      args: {
+        take: intArg({ default: 10 }),
+        signer: stringArg(),
+        skip: intArg(),
+        order: arg({
+          type: Order,
+          default: 'asc'
+        })
+      },
+      async resolve (p, args, ctx) {
+        const { take, skip, order, signer } = args
+        const count = await ctx.prisma.block.count({
+          where: {
+            signer
+          }
+        })
+        const list = await ctx.prisma.block.findMany({
+          where: {
+            signer
+          },
+          include:{
+            txs: true
+          },
+          orderBy: {
+            number: order
+          },
+          take,
+          skip
+        })
+
+        return {
+          count,
+          list
+        }
+      }
+    })
+  },
 })
